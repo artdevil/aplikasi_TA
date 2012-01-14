ca592e1c9e411c16cf252a553724b67e5bf668be
art devil <artdvl91@gmail.com>
2012-01-14 11:12:31 +0700

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AplikasiTA::Application.config.secret_token = '90d4f35c19008729ca0cc1b1c15adb52b3aa953e784718530dc3b291b817253fba1e39e10e10ca7e4b9383741d65ce1311dbdfa9eedca91e5a918c0dab0e48d6'
