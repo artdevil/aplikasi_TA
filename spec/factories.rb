8b6513b41bd86051356136f2626fede8cc181bdb
art devil <artdvl91@gmail.com>
2012-01-16 09:24:24 +0700

    start to make sessions

---
+++ spec/factories.rb
@@ -0,0 +1,7 @@
+Factory.define :lecture do |lecture|
+          lecture.NIP                     "611090096" 
+          lecture.kode_dosen              "AGS"
+          lecture.nama_dosen              "Adhiguna Utama Sabril"
+          lecture.password                "foobar"
+          lecture.password_confirmation   "foobar"
+end
